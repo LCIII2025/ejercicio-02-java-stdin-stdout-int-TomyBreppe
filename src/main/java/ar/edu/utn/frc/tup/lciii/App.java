1ba7411b9afadd92e586f109e82a790b54288062
113912_BREPPE_TOMAS <113912@tecnicatura.frc.utn.edu.ar>
2025-04-10 20:33:18 -0300

    First commit

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -12,10 +12,14 @@ public class App
     {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
         // Complete this line
         // Complete this line
 
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         // Complete this line
         // Complete this line
 
